63bdac444d4686910547c5f4555e978f93899b90
Ruirong Xiang <rxiang25@ilb.cat>
2026-03-17 14:00:57 +0100

    Exercici8 fet

--- RA3Ex7-XiangTianRuirong.js
+++ RA3Ex7-XiangTianRuirong.js
@@ -41,5 +41,8 @@ botoAfegir.addEventListener("click", function(){
 })
 
 //8 - Eliminació: Fes que un element desaparegui en fer-li clic usant el mètode remove().
-
-
+let elementEliminar = document.getElementById("element-eliminar")
+elementEliminar.addEventListener("click", function(){ 
+    elementEliminar.remove()
+    
+})
