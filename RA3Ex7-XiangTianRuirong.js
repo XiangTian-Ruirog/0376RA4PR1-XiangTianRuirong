327e6434d469d4bb0111c23b85f54f6308b32f2c
Ruirong Xiang <rxiang25@ilb.cat>
2026-03-17 13:49:23 +0100

    Exercici1 fet

---
+++ RA3Ex7-XiangTianRuirong.js
@@ -0,0 +1,5 @@
+//RA3Ex7 - Activitats ràpides DOM JS
+
+//1 - Selecció: Usa querySelector per canviar el color d'un <h1> quan la pàgina carregui.
+let h = document.querySelector("#titol-principal")
+h.style.color="green"
