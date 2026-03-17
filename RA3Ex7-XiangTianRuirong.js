72658d26740de16fa7387399ec167f255041afbd
Ruirong Xiang <rxiang25@ilb.cat>
2026-03-17 13:49:48 +0100

    Exercici2 fet

--- RA3Ex7-XiangTianRuirong.js
+++ RA3Ex7-XiangTianRuirong.js
@@ -3,3 +3,8 @@
 //1 - Selecció: Usa querySelector per canviar el color d'un <h1> quan la pàgina carregui.
 let h = document.querySelector("#titol-principal")
 h.style.color="green"
+
+//2 - Contingut: Fes que un paràgraf <p> mostri "Hola Món" usant textContent.
+let p = document.getElementById("paragraf-hola")
+p.textContent="Hola Món"
+
